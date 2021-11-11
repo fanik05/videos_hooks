4f786506489eeed5d1621d714e1e191576b0dd26
fanik05 <fatinanik129@gmail.com>
2021-11-11 12:44:23 +0600

    custom hook added

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,32 +1,28 @@
-import { Component } from 'react'
+import { useState } from 'react'
 
-class SearchBar extends Component {
-    state = { term: '' }
+const SearchBar = ({ onFormSubmit }) => {
+    const [term, setTerm] = useState('')
 
-    onInputChange = e => this.setState({ term: e.target.value })
+    const onSubmit = event => {
+        event.preventDefault()
 
-    onFormSubmit = e => {
-        e.preventDefault()
-
-        this.props.onFormSubmit(this.state.term)
+        onFormSubmit(term)
     }
 
-    render() {
-        return (
-            <div className="search-bar ui segment">
-                <form onSubmit={this.onFormSubmit} className="ui form">
-                    <div className="field">
-                        <label>Video Search</label>
-                        <input
-                            type="text"
-                            value={this.state.term}
-                            onChange={this.onInputChange}
-                        />
-                    </div>
-                </form>
-            </div>
-        )
-    }
+    return (
+        <div className="search-bar ui segment">
+            <form onSubmit={onSubmit} className="ui form">
+                <div className="field">
+                    <label>Video Search</label>
+                    <input
+                        type="text"
+                        value={term}
+                        onChange={event => setTerm(event.target.value)}
+                    />
+                </div>
+            </form>
+        </div>
+    )
 }
 
 export default SearchBar
